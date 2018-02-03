1dcdf88e8579afa90cd8aec634a5aa2e8faaf24c
maldiohead <maldiohead@outlook.com>
2018-02-03 23:21:14 +0800

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -32,6 +32,7 @@ extern "C" {
     #include "jh.h"
     #include "x5.h"
     #include "c11.h"
+    #include"blake2.h"
 }
 
 #include "boolberry.h"
@@ -352,7 +353,30 @@ Handle<Value> groestlmyriad(const Arguments& args) {
     Buffer* buff = Buffer::New(output, 32);
     return scope.Close(buff->handle_);
 }
+}
 
+
+Handle<Value> blake2(const Arguments& args) {
+    HandleScope scope;
+
+    if (args.Length() < 1)
+        return except("You must provide one argument.");
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!Buffer::HasInstance(target))
+        return except("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char output[32];
+
+    uint32_t input_len = Buffer::Length(target);
+
+    blake2_hash(input, output, input_len);
+
+    Buffer* buff = Buffer::New(output, 32);
+    return scope.Close(buff->handle_);
+}
 
 Handle<Value> blake(const Arguments& args) {
     HandleScope scope;
